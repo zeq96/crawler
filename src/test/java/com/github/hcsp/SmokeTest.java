914523e410b463fb9144c5a7245c5d2a155c552a
AndrewZ <zeq_96@163.com>
2021-07-21 15:19:24 +0800

    修改测试代码

--- src/test/java/com/github/hcsp/SmokeTest.java
+++ src/test/java/com/github/hcsp/SmokeTest.java
@@ -1,36 +1,10 @@
 package com.github.hcsp;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.ZoneId;
-import java.util.List;
 
 public class SmokeTest {
     @Test
     public void smoke() {
-
-    }
-
-    @Test
-    public void getInstantTest() {
-        String resource = "db/mybatis/config.xml";
-        SqlSessionFactory sqlSessionFactory;
-        try {
-            InputStream inputStream = Resources.getResourceAsStream(resource);
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            List<News> currentNews = sqlSession.selectList("com.github.hcsp.MockMapper.selectTheFirst500News");
-            currentNews.forEach(news -> System.out.println(news.getCreatedAt().atZone(ZoneId.of("GMT+8")) + "," + news.getModifiedAt()));
-        }
 
     }
 
